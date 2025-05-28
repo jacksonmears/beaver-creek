28f4e6701155866ef16e9499ea58424ecc28c2d3
Jackson Mears <jackson.mears2002@gmail.com>
2025-05-27 23:32:00 -0500

    commit #8
    we're really getting somewhere here man

--- src/components/NewHeader.tsx
+++ src/components/NewHeader.tsx
@@ -1,119 +1,86 @@
 import React from 'react';
 import './css/Header.css';
-
+import { StyleSheet } from '@react-pdf/renderer'
 
 const PRIMARY_COLOR = "#00674F";
-// const PRIMARY_COLOR = "#50C878";
 const SECONDARY_COLOR = "#000000";
 const P_FONTSIZE = 15;
 const bookNowButtonPaddingSize = 5;
 
-
 const Header: React.FC = () => (
-    <div style={headerContainer}>
-        <p style={courseTitle}>Beaver Creek Golf Club</p>
-        <div style={navBarContainer}>
-            <div className="navItem" style={navBarButtonContainer}>
-                <p style={navBarText}>Rates</p>
+    <div style={styles.headerContainer}>
+
+
+
+        <div className="box" style={styles.navBarContainer}>
+            <div className="navItem" style={styles.navBarButtonContainer}><p style={styles.navBarText}>Rates</p></div>
+            <div className="navItem" style={styles.navBarButtonContainer}><p style={styles.navBarText}>Course
+                Details</p></div>
+            <div className="navItem" style={styles.navBarButtonContainer}><p style={styles.navBarText}>Pass Programs</p>
             </div>
-            <div className="navItem" style={navBarButtonContainer}>
-                <p style={navBarText}>Course Details</p>
+            <div className="navItem" style={styles.navBarButtonContainer}><p style={styles.navBarText}>Golf
+                Instruction</p></div>
+            <div className="navItem" style={styles.navBarButtonContainer}><p style={styles.navBarText}>Event
+                Creation</p></div>
+            <div className="navItem" style={styles.navBarButtonContainer}><p style={styles.navBarText}>League
+                Information</p></div>
+            <div className="navItem" style={styles.navBarButtonContainer}><p style={styles.navBarText}>Club House</p>
             </div>
-            <div className="navItem" style={navBarButtonContainer}>
-                <p style={navBarText}>Pass Programs</p>
+            <div className="navItem" style={styles.navBarButtonContainer}><p style={styles.navBarText}>Contact Us</p>
             </div>
-            <div className="navItem" style={navBarButtonContainer}>
-                <p style={navBarText}>Golf Instruction</p>
+            <div className="classButton" style={styles.bookTimeButtonContainer}><p style={styles.titleText}>Book Now</p>
             </div>
-            <div className="navItem" style={navBarButtonContainer}>
-                <p style={navBarText}>Even Creation</p>
-            </div>
-            <div className="navItem" style={navBarButtonContainer}>
-                <p style={navBarText}>League Information</p>
-            </div>
-            <div className="navItem" style={navBarButtonContainer}>
-                <p style={navBarText}>Club House</p>
-            </div>
-            <div className="navItem" style={navBarButtonContainer}>
-                <p style={navBarText}>Contact Us</p>
-            </div>
-            <div className="classButton" style={bookTimeButtonContainer}>
-                <p style={titleText}>Book Now</p>
-            </div>
-            <div className="navItem" style={navBarButtonContainer}>
-                <p style={navBarText}>Login</p>
-            </div>
+            <div className="navItem" style={styles.navBarButtonContainer}><p style={styles.navBarText}>Login</p></div>
         </div>
     </div>
 );
 
 export default Header;
 
+const styles = StyleSheet.create({
+    headerContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+    },
+    navBarContainer: {
+        marginLeft: '700px',
+        display: 'flex',
+        flexDirection: 'row',
+        position: 'relative',
+        padding: '5px',
+        backgroundColor: SECONDARY_COLOR,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: '0.1px',
+        borderColor: PRIMARY_COLOR,
+        borderStyle: "solid",
+        borderRadius: '5px',
+        boxShadow: '0px 2px 4px 10px rgba(80, 200, 120, 0.2)',
+    },
+    bookTimeButtonContainer: {
+        borderRadius: '20px',
+        paddingLeft: bookNowButtonPaddingSize,
+        paddingRight: bookNowButtonPaddingSize,
+        paddingTop: bookNowButtonPaddingSize / 2,
+        paddingBottom: bookNowButtonPaddingSize / 2,
+        marginLeft: '10px',
+    },
+    titleText: {
+        fontFamily: "Cursive, sans-serif",
+        fontWeight: "bold",
+        fontSize: P_FONTSIZE * 1.3,
+    },
+    navBarText: {
+        color: PRIMARY_COLOR,
+        fontSize: P_FONTSIZE,
+        fontFamily: "Garamond, sans-serif",
+        fontWeight: "bold",
+    },
+    navBarButtonContainer: {
+        paddingTop: '10px',
+        paddingBottom: '10px',
+        marginLeft: '10px',
+    },
 
-const headerContainer: React.CSSProperties = {
-    display: 'flex',
-    flexDirection: 'row',
-}
-
-const navBarContainer: React.CSSProperties = {
-    display: 'flex',
-    flexDirection: 'row',
-    position: 'relative',
-    padding: '5px',
-    // backgroundImage: `url('/src/assets/images/homepage-header-image.png')`,
-    // backgroundSize: 'cover',
-    // backgroundPosition: 'center',
-    // backgroundRepeat: 'no-repeat',
-    backgroundColor: SECONDARY_COLOR,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: '0.1px',
-    borderColor: PRIMARY_COLOR,
-    borderStyle: "solid",
-    borderRadius: '5px',
-    boxShadow: '0px 2px 4px 10px rgba(80, 200, 120, 0.2)', // horizontal, vertical, blur, color
-};
-
-const bookTimeButtonContainer: React.CSSProperties = {
-    // backgroundColor: PRIMARY_COLOR,
-    borderRadius: '20px',
-    paddingLeft: bookNowButtonPaddingSize,
-    paddingRight: bookNowButtonPaddingSize,
-    paddingTop: bookNowButtonPaddingSize / 2,
-    paddingBottom: bookNowButtonPaddingSize / 2,
-    marginLeft: '10px',
-}
-
-const titleText: React.CSSProperties = {
-    // color: SECONDARY_COLOR,
-    fontFamily: "Roboto, sans-serif",
-    fontWeight: "bold",
-    fontSize: P_FONTSIZE * 1.3,
-}
-
-const navBarText: React.CSSProperties = {
-    color: PRIMARY_COLOR,
-    fontSize: P_FONTSIZE,
-    // marginRight: '15px',
-    fontFamily: "Montserrat, sans-serif",
-}
-
-const navBarButtonContainer: React.CSSProperties = {
-    // borderColor: PRIMARY_COLOR,
-    // borderWidth: '1px',
-    // borderStyle: 'solid',
-    paddingTop: '10px',
-    paddingBottom: '10px',
-    marginLeft: '10px',
-}
-
-
-const courseTitle: React.CSSProperties = {
-    fontFamily: "Roboto, sans-serif",
-    fontWeight: "bold",
-    fontSize: P_FONTSIZE*2,
-    color: "white",
-    marginRight: '200px',
-}
-
-
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import NewHeader from "./components/NewHeader.tsx";
-import React from 'react';
 import headerImage from './assets/images/homepage-header-image.png';
+import { StyleSheet } from '@react-pdf/renderer'
+
+const PRIMARY_COLOR = "#00674F";
+const P_FONTSIZE = 15;
 
 function Index() {
 
     return (
-        <div style={{justifyItems: 'center', backgroundColor: 'black', padding: '20px'}}>
-            <NewHeader />
-            <div style={{padding: '20px'}}></div>
+        <div style={{justifyItems: 'center', padding: '20px'}}>
+            <NewHeader/>
+            <div style={styles.titleContainer}>
+                <p style={styles.courseTitle}>Beaver Creek Golf Club</p>
+                <p style={styles.courseTitleDescription}>The only thing a golfer needs is more daylight</p>
+
+            </div>
             <div style={{width: '100%', height: '2000px'}}>
-                <img src={headerImage} alt={"scenic picture of golf course"} style={pictureStyle}/>
+            <img src={headerImage} alt={"scenic picture of golf course"} style={styles.pictureStyle}/>
 
             </div>
 
@@ -20,8 +27,30 @@ function Index() {
 export default Index;
 
 
+const styles = StyleSheet.create({
+    pictureStyle: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        zIndex: -1,
+    },
+    titleContainer: {
+        position: 'absolute',
+        flexShrink: 0,
+        marginRight: '30px',
+        top: '100px'
+    },
+    courseTitle: {
+        fontFamily: "ICA Rubrik, sans-serif",
+        fontWeight: "bold",
+        fontSize: P_FONTSIZE * 6,
+        color: "transparent",
+        WebkitTextStroke: `3px ${PRIMARY_COLOR}`,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: PRIMARY_COLOR,
+    },
 
-const pictureStyle: React.CSSProperties = {
-    // width: '100%',
-    // height: '400px'
-}
+
+})
