9c5945bb97c68279d8f4cc02f79ecb294d71033f
Jackson Mears <jackson.mears2002@gmail.com>
2025-05-27 19:44:33 -0500

    commit #6
    closer but still not very pleasing on the eye

--- src/components/NewHeader.tsx
+++ src/components/NewHeader.tsx
@@ -1,23 +1,75 @@
 import React from 'react';
+import './css/Header.css';
+
+
+// const PRIMARY_COLOR = "#00674F";
+const PRIMARY_COLOR = "#50C878";
+const SECONDARY_COLOR = "#000000";
+const P_FONTSIZE = 15;
+const bookNowButtonPaddingSize = 5;
 
 
 const Header: React.FC = () => (
     <div style={headerStyles}>
-        <p>testing</p>
+        <p className="navItem" style={navBarText}>Rates</p>
+        <p className="navItem" style={navBarText}>Course Details</p>
+        <p className="navItem" style={navBarText}>Pass Programs</p>
+        <p className="navItem" style={navBarText}>Golf Instruction</p>
+        <p className="navItem" style={navBarText}>Event Creation</p>
+        <p className="navItem" style={navBarText}>League Information</p>
+        <p className="navItem" style={navBarText}>Club House</p>
+        <p className="navItem" style={navBarText}>Contact Us</p>
+        <div className="classButton" style={bookTimeButtonContainer}>
+            <p style={titleText}>Book Now</p>
+        </div>
+        <p className="navItem" style={{...navBarText, marginLeft: '15px'}}>Login</p>
     </div>
 );
 
 export default Header;
 
 
-
 const headerStyles: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'row',
     position: 'relative',
-    width: '100%',
-    height: '400px',
-    backgroundImage: `url('/src/assets/images/homepage-header-image.png')`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
+    padding: '5px',
+    // backgroundImage: `url('/src/assets/images/homepage-header-image.png')`,
+    // backgroundSize: 'cover',
+    // backgroundPosition: 'center',
+    // backgroundRepeat: 'no-repeat',
+    backgroundColor: SECONDARY_COLOR,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: '2px',
+    borderColor: PRIMARY_COLOR,
+    borderStyle: "solid",
+    borderRadius: '5px',
+    boxShadow: '0px 2px 4px 10px rgba(80, 200, 120, 0.2)', // horizontal, vertical, blur, color
 };
+
+const bookTimeButtonContainer: React.CSSProperties = {
+    // backgroundColor: PRIMARY_COLOR,
+    borderRadius: '20px',
+    paddingLeft: bookNowButtonPaddingSize,
+    paddingRight: bookNowButtonPaddingSize,
+    paddingTop: bookNowButtonPaddingSize/2,
+    paddingBottom: bookNowButtonPaddingSize/2,
+}
+
+const titleText: React.CSSProperties = {
+    // color: SECONDARY_COLOR,
+    fontFamily: "Roboto, sans-serif",
+    fontWeight: "bold",
+    fontSize: P_FONTSIZE*1.3,
+}
+
+const navBarText: React.CSSProperties = {
+    color: PRIMARY_COLOR,
+    fontSize: P_FONTSIZE,
+    marginRight: '15px',
+    fontFamily: "Montserrat, sans-serif",
+}
+
+
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import NewHeader from './components/Header'
+import NewHeader from "./components/NewHeader.tsx";
+import React from 'react';
+import headerImage from './assets/images/homepage-header-image.png';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <NewHeader />
-  </StrictMode>,
-)
+function Index() {
+
+    return (
+        <div style={{justifyItems: 'center', backgroundColor: 'black', padding: '20px'}}>
+            <NewHeader />
+            <div style={{padding: '20px'}}></div>
+            <div style={{width: '100%', height: '2000px'}}>
+                <img src={headerImage} alt={"scenic picture of golf course"} style={pictureStyle}/>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Index;
+
+
+
+const pictureStyle: React.CSSProperties = {
+    // width: '100%',
+    // height: '400px'
+}
