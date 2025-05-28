648173e111135c786f5025e0fd3cd2b557c1a11d
Jackson Mears <jackson.mears2002@gmail.com>
2025-05-27 23:39:08 -0500

    commit #9
    added title description and changed the formatting!

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,30 @@
 import NewHeader from "./components/NewHeader.tsx";
 import headerImage from './assets/images/homepage-header-image.png';
-import { StyleSheet } from '@react-pdf/renderer'
+import {StyleSheet} from "@react-pdf/renderer";
 
 const PRIMARY_COLOR = "#00674F";
 const P_FONTSIZE = 15;
 
 function Index() {
-
     return (
-        <div style={{justifyItems: 'center', padding: '20px'}}>
-            <NewHeader/>
+        <div style={{ padding: '20px', position: 'relative' }}>
+            <NewHeader />
             <div style={styles.titleContainer}>
                 <p style={styles.courseTitle}>Beaver Creek Golf Club</p>
-                <p style={styles.courseTitleDescription}>The only thing a golfer needs is more daylight</p>
-
+                <p style={styles.courseTitleDescription}>A timeless dance of turf, light, and sky</p>
             </div>
-            <div style={{width: '100%', height: '2000px'}}>
-            <img src={headerImage} alt={"scenic picture of golf course"} style={styles.pictureStyle}/>
-
+            <div style={{ width: '100%', height: '2000px' }}>
+                <img
+                    src={headerImage}
+                    alt={"scenic picture of golf course"}
+                    style={styles.pictureStyle}
+                />
             </div>
-
         </div>
-    )
+    );
 }
 
 export default Index;
-
 
 const styles = StyleSheet.create({
     pictureStyle: {
@@ -36,10 +35,10 @@ const styles = StyleSheet.create({
         zIndex: -1,
     },
     titleContainer: {
-        position: 'absolute',
-        flexShrink: 0,
-        marginRight: '30px',
-        top: '100px'
+        marginTop: '40px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
     },
     courseTitle: {
         fontFamily: "ICA Rubrik, sans-serif",
@@ -47,10 +46,14 @@ const styles = StyleSheet.create({
         fontSize: P_FONTSIZE * 6,
         color: "transparent",
         WebkitTextStroke: `3px ${PRIMARY_COLOR}`,
-        borderWidth: 1,
-        borderStyle: 'solid',
-        borderColor: PRIMARY_COLOR,
+        margin: 0,
     },
-
-
-})
+    courseTitleDescription: {
+        fontFamily: "ICA Rubrik, sans-serif",
+        fontSize: P_FONTSIZE * 1.5,
+        color: PRIMARY_COLOR,
+        fontWeight: "bold",
+        margin: 0,
+        marginTop: '10px'
+    }
+});
